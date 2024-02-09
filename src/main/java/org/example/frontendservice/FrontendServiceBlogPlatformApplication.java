df4584f9bce9cd5a2ef5d1beea42069c9377e187
GorbunovIvan <gorbunovvivan@gmail.com>
2024-02-09 09:19:18 +0200

    init

---
+++ src/main/java/org/example/frontendservice/FrontendServiceBlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.frontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendServiceBlogPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendServiceBlogPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/frontendservice/FrontendServiceBlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.frontendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendServiceBlogPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
